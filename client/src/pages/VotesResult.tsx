6c7be29f16120e7c341278d95d6ae2f769bf7389
Florian <53081078+Flo-CS@users.noreply.github.com>
2023-10-15 17:26:56 +0200

    🐛 fix votes result screen

--- client/src/pages/VotesResult.tsx
+++ client/src/pages/VotesResult.tsx
@@ -33,36 +33,44 @@ export default function VotesResult() {
     fetchProposals();
   }, [contract]);
 
+  const shownProposals = proposals.filter(
+    (proposal) => proposal.id !== winningProposal?.id
+  );
+
   return (
     <div className="flex flex-col items-center mt-12 space-y-8">
       <hr className="w-full" />
       <h3 className="text-3xl font-semibold">Votes result</h3>
-      <p className="text-xl">This is the winning proposal !</p>
       {winningProposal && (
-        <Proposal
-          description={winningProposal.description}
-          id={winningProposal.id}
-          votesCount={winningProposal.votesCount}
-          showVotesCount={true}
-          isWinning={true}
-        />
+        <>
+          <p className="text-xl">This is the winning proposal !</p>
+          <Proposal
+            description={winningProposal.description}
+            id={winningProposal.id}
+            votesCount={winningProposal.votesCount}
+            showVotesCount={true}
+            isWinning={true}
+          />
+        </>
       )}
-      <p className="text-xl">And the votes for all others proposals</p>
-      <ProposalsContainer>
-        {proposals
-          .filter((proposal) => proposal.id !== winningProposal?.id)
-          .map((proposal) => {
-            return (
-              <Proposal
-                key={proposal.id}
-                description={proposal.description}
-                id={proposal.id}
-                votesCount={proposal.votesCount}
-                isWinning={winningProposal?.id === proposal.id}
-              />
-            );
-          })}
-      </ProposalsContainer>
+      {shownProposals.length !== 0 && (
+        <>
+          <p className="text-xl">And the votes for all others proposals</p>
+          <ProposalsContainer>
+            {shownProposals.map((proposal) => {
+              return (
+                <Proposal
+                  key={proposal.id}
+                  description={proposal.description}
+                  id={proposal.id}
+                  votesCount={proposal.votesCount}
+                  isWinning={winningProposal?.id === proposal.id}
+                />
+              );
+            })}
+          </ProposalsContainer>
+        </>
+      )}
     </div>
   );
 }
